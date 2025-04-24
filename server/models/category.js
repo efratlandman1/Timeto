289a340f02fd99c1a44f390ddcd875d66ae0e450
mlandman <mlandman@golantelecom.co.il>
2025-04-24 15:54:11 +0300

    Get categories data - client side

--- server/models/category.js
+++ server/models/category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
-    category: { type: String, required: true },
+    name: { type: String, required: true },
     subcategories: [{
         name: { type: String, required: true }
     }]
