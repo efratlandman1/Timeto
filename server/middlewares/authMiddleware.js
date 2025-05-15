f85256af2928c995f59c75587bac91786464320f
Efrat <efch99@gmail.com>
2025-05-15 18:10:36 +0300

    add new service api to open API"

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+
+// פונקציה לבדיקת התאמה של מסלול עם פרמטרים דינמיים
+const matchPath = (path, routePattern) => {
+  const routeRegex = new RegExp('^' + routePattern.replace(/:[^\/]+/g, '[^/]+') + '$');
+  return routeRegex.test(path);
+};
+
 const jwtAuthMiddleware = (req, res, next) => {
     const openRoutes = [
         '/api/v1/login',
         '/api/v1/register',
         '/api/v1/businesses',
-        '/api/v1/categories'
-
-
+        '/api/v1/categories',
+        '/api/v1/services/byCategory/:categoryId'
     ];
 
-    if (openRoutes.includes(req.path) ||
-        /^\/api\/v1\/services\/byCategory\/[^\/]+$/.test(req.path)) {  //delete!!!!!!!!!!!!!!
-        return next(); // Allow access to open routes
+    // if (openRoutes.includes(req.path) 
+    // ) {  //delete!!!!!!!!!!!!!!
+    //     return next(); // Allow access to open routes
+    // }
+    const isOpen = openRoutes.some((route) => matchPath(req.path, route));
+    if (isOpen) {
+        return next(); // פתוח – לא צריך טוקן
     }
 
     const token = req.header('Authorization')?.split(' ')[1]; // Expect "Bearer <token>"
